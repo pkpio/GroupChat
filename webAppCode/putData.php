535478196f66bb74c7a9e3fef67a96fd27ec1c5e
Praveen Kumar Pendyala <praveendath92@gmail.com>
2013-10-06 22:32:07 -0700

    Support for removal of not allowed chars
    Added the removal of not allowed chars in the web app.
    praveen with anonym. fuck with f**k for now. More may follow in future.

--- webAppCode/putData.php
+++ webAppCode/putData.php
@@ -1,8 +1,17 @@
 <?php
 $MessagesFile = 'messages.txt';
 $handle = fopen($MessagesFile, 'r') or die('Cannot open file:  ' . $MessagesFile);
-$message = fread($handle, filesize($MessagesFile));
-$message = $message . $_GET['value'] . "\n";
+$message     = fread($handle, filesize($MessagesFile));
+$message     = $message . $_GET['value'] . "\n";
+$notAlloweds = array(
+    'praveen',
+    'fuck'
+);
+$replacers   = array(
+    'anonym',
+    'f**k'
+);
+$message     = str_replace($notAlloweds, $replacers, $message);
 fclose($handle);
 $handle2 = fopen($MessagesFile, 'w') or die('Cannot open file:  ' . $MessagesFile);
 fwrite($handle2, $message);
